b106c1aab5d206baf6c6467af5309b4795328fe0
Kit Fuderich <kitfuderich@gmail.com>
2022-01-31 11:09:53 -0800

    material-ui core added and simple styling to header

--- front_end/src/components/Header.tsx
+++ front_end/src/components/Header.tsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import { useEthers } from '@usedapp/core';
+import { Button, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        padding: theme.spacing(4),
+        theme: 'flex',
+        justifyContent: "flex-end",
+        gap: theme.spacing(1)
+    }
+}))
 
 const Header = () => {
+
+    const classes = useStyles()
 
     const { account, activateBrowserWallet, deactivate } = useEthers()
 
     const isConnected = account !== undefined
 
     return (
-        <div>
-            {
-                isConnected ? (<button color="primary" onClick={deactivate}>Disconnect</button>) :
-                    (<button color="primary" onClick={activateBrowserWallet}>Connect</button>)
-            }
+        <div className={classes.container}>
+            <div>
+                {
+                    isConnected ? (<button color="primary" onClick={deactivate}>Disconnect</button>) :
+                        (<button color="primary" onClick={activateBrowserWallet}>Connect</button>)
+                }
+            </div>
         </div>
     )
-
-
 
 };
 
